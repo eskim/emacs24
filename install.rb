d7f2ec79dcd62bcdb489f6b01f2f35660296957a
Eunsub Kim <eunsub@gmail.com>
2011-12-07 13:36:55 +0900

    fix installer

--- install.rb
+++ install.rb
@@ -2,10 +2,11 @@ require 'fileutils'
 
 
 Link = lambda do |target, new|
-  FileUtils.ln_s File.join(File.expand_path(File.dirname(__FILE__)), target), File.join(File.expand_path('~'), new) rescue puts "~/#{new} exists."
+  FileUtils.ln_sf File.join(File.expand_path(File.dirname(__FILE__)), target), File.join(File.expand_path('~'), new) rescue puts "~/#{new} exists."
 end
 
 
 Link['init.el', '.emacs.d/init.el']
+Link['custom.el', '.emacs.d/custom.el']
 Link['eskim', '.emacs.d/eskim']
 
